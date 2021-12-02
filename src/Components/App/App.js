4a6db32927e1ed8318da9e426da1de668bc09f95
Dave Sanders <davepaulsanders@gmail.com>
2021-12-02 10:11:34 -0500

    Added async function inside a new  useEffect hood and added a useState hook

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,33 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Scoreboard } from "../Scoreboard/Scoreboard";
 import { Ballislife } from "../../util/Ballislife";
-export class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchResults: [],
-    };
-  }
-  async componentDidMount() {
-    let gamesToday = await Ballislife.search();
-    // The data is  imported from Ballislife here and mapped to each game.
+export const App = () => {
+  //Constructor is removed and replaced with functional component with empty state and set state
+  const [gamesToday, setGamesToday] = useState([]);
 
-    gamesToday = gamesToday.map((game) => ({
-      hometeam: game.home_team.name,
-      hometeamscore: game.home_team_score,
-      awayteam: game.visitor_team.name,
-      awayteamscore: game.visitor_team_score,
-      status: game.status,
-    }));
-    this.setState({ searchResults: gamesToday });
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1 className="title">NBA SCORES</h1>
-        <Scoreboard scores={this.state.searchResults} />
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    async function getGames() {
+      let searchGames = await Ballislife.search()(
+        //I think i have to call the getGames earlier maybe?
+        // The data is  imported from Ballislife here and mapped to each game.
+
+        (searchGames = searchGames.map((game) => ({
+          hometeam: game.home_team.name,
+          hometeamscore: game.home_team_score,
+          awayteam: game.visitor_team.name,
+          awayteamscore: game.visitor_team_score,
+          status: game.status,
+        })))
+      );
+    }
+    gamesToday();
+    setGamesToday(searchGames);
+  }, []); // Something like this so it only reloads when those
+  // specific score change
+  return (
+    <div className="App">
+      <h1 className="title">NBA SCORES</h1>
+      {/* This should pass down the new state that I created */}
+      <Scoreboard scores={gamesToday} />
+    </div>
+  );
+};
